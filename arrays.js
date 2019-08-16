19e847187e5d8e52ee81d414f90adfb48307d9d7
Travis Clarke <spardact@gmail.com>
2019-08-16 20:47:57 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -27,10 +27,6 @@ function removeElementFromBeginningOfArray(array) {
   return array.slice(1)
 }
 
-function destructivelyRemoveElementFromEndOfArray(array) {
-  array.pop(1, 2, 3)
-  return array
-}
 
 function removeElementFromEndOfArray(array) {
   return array.slice(0, array.length - 1)
