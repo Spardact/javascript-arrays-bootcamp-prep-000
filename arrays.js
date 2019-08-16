07b31fe0f86e27e4559c794369eb0ce0aa3a2684
Travis Clarke <spardact@gmail.com>
2019-08-16 20:09:19 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,3 +1,1 @@
-var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
-
-var addElementToBeginningOfArray = 
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles
